0e3743c3ebecfc14ec64d3b7abc8d0b4c0968463
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-07 19:37:29 -0600

    Added Admin area, new controllers, and namespace to routes

--- blog/App_Start/RouteConfig.cs
+++ blog/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using blog.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,9 +12,13 @@ namespace blog
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var namespaces = new[] { typeof(PostsController).Namespace };
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" });
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces);
+
         }
     }
 }
